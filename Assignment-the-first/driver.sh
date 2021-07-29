483c106db47c451b86e2b97ec383b798599fd16e
Brian Palmer <bpalmer3@uoregon.edu>
2021-07-28 22:06:40 -0700

    work for distribution charts

---
+++ Assignment-the-first/driver.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --output="file_%j.out"
+#SBATCH --error="file_%j.err"
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+
+/usr/bin/time -v ./first.py $1
---
+++ Assignment-the-first/first.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+
+import gzip
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def convert_phred(letter):
+    """Converts a single character into a phred score"""
+    return ord(letter) - 33
+
+
+def file_line_generator(filename: str, n_lines: int = 1) -> None:
+    '''
+    filename: path to file
+    n_lines: number of lines in the list to read
+    '''
+    fh = gzip.open(filename, 'rb')
+    #fh = open(filename, 'rb')
+    lines: list = []
+    ln: int = 0
+    for line in fh:
+        ln += 1
+        lines.append(line.strip())
+        if ln % n_lines == 0:
+            yield lines
+            lines.clear()
+    
+    fh.close()
+
+
+N_LINES_IN_FASTQ_RECORD: int = 4
+def get_fastq_records(filename: str):
+    '''
+    TODO documentation
+    '''
+
+    for record in file_line_generator(filename, N_LINES_IN_FASTQ_RECORD):
+        yield record
+
+
+# /projects/bgmp/shared/2017_sequencing/
+# /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz
+PARENT_DIR='/projects/bgmp/shared/2017_sequencing/'
+def populate_list_of_files(files:list):
+	for i in range(1,5):
+		files.append(f"{PARENT_DIR}1294_S1_L008_R{i}_001.fastq.gz")
+
+
+# same number of lines in R1-R4
+n_records: int = 1452986940 / 4
+n_basepairs: int = 101
+
+if "__main__" == __name__:
+
+    filename = sys.argv[1]
+    x = np.zeros(n_basepairs, dtype=float)
+    for record in get_fastq_records(filename):
+        x += [(c - 33) for c in record[1]]
+
+    for i in range(len(x)):
+        x[i] /= n_records
+
+    print(x)
+    plt.bar([i for i in range(101)], x, ec='blue')
+    plt.title(f"{filename[filename.rfind('/') + 1:filename.rfind('.')]} Average Quality Score per Base Pair")
+    plt.xlabel("Base Pair Position")
+    plt.ylabel("Mean Quality Score")
+    plt.savefig(f"{filename[filename.rfind('/') + 1:filename.rfind('.')]}.png")
+
